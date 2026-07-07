43cc23ad4a3eea155b62de4a5129a51a2c5f0903
patrick <patrick@qq.com>
2026-07-07 19:50:58 +0800

    类型别名与类型定义

---
+++ go语言基本语法/类型别名与类型定义.go
@@ -0,0 +1,9 @@
+package main
+
+// 类型别名的语法格式
+// type 新类型名 = 已有类型名
+
+// 具体使用方法
+type newstring = string // 定义了一个新的类型别名 newsstring，它与 string 类型完全相同
+
+type stringAlias string // 定义了一个新的类型别名 stringAlias，它与 string 类型完全相同
