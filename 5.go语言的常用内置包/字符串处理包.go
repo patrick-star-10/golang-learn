6383fef98aa09481138bc1f58e25b6fcdce1221a
patrick <patrick@qq.com>
2026-07-28 16:52:11 +0800

    unicode/utf8包

---
+++ 5.go语言的常用内置包/字符串处理包.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+// 字符串的遍历包括按照字节遍历和按字符遍历
+func Unicode() {
+	s := "我爱go语言"
+	fmt.Println("字节长度：", len(s))
+	fmt.Println("-----------------")
+	// for...range遍历字符串
+	len := 0
+	for i, ch := range s {
+		fmt.Printf("%d:%c", i, ch)
+		len++
+	}
+	fmt.Println("\n字符串长度", len)
+	fmt.Println("-----------------")
+	// 遍历所有字节
+	for i, ch := range []byte(s) {
+		fmt.Printf("%d:%x ", i, ch)
+	}
+	fmt.Println()
+	fmt.Println("-----------------")
+	// 遍历所有字符
+	count := 0
+	for i, ch := range []rune(s) {
+		fmt.Printf("%d:%c", i, ch)
+		count++
+	}
+	fmt.Println()
+	fmt.Println("字符串长度：", count)
+	fmt.Println("字符串长度", utf8.RuneCountInString(s))
+}
