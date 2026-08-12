bd1f5ee49212106c5fe2d3b129ba80bf9545849f
patrick <patrick@qq.com>
2026-08-12 16:14:06 +0800

    DeepAndShallowCopy

--- 6.go语言面向对象编程/结构体struct.go
+++ 6.go语言面向对象编程/结构体struct.go
@@ -131,3 +131,48 @@ func changeName(h human) {
 	h.age = 13
 	fmt.Printf("函数体内修改后:%T,%v,%p \n", h, h, &h) // h是新的结构体，不影响h1
 }
+
+// 结构体的深拷贝和浅拷贝
+// 值类型是深拷贝，深拷贝就是为新的对象非配了内存。引用类型就是浅拷贝，浅拷贝只是复制了对象的指针。
+type Dog struct {
+	name  string
+	color string
+	age   int8
+	kind  string
+}
+
+func DeepAndShallowCopy() {
+	//1.实现结构体深拷贝
+	// struct是值类型，默认的复制就是身拷贝
+	d1 := Dog{"一二", "白色", 2, "熊猫"}
+	fmt.Printf("d1:%T,%v,%p \n", d1, d1, &d1)
+	d2 := d1 //深拷贝
+	fmt.Printf("d2:%T,%v,%p \n", d2, d2, &d2)
+	d2.name = "布布"
+	fmt.Println("d2修改后: ", d2)
+	fmt.Println("d1: ", d1)
+	fmt.Println("------------")
+	//2.实现结构体浅拷贝：直接赋值指针地址
+	d3 := &d1
+	fmt.Printf("d3:%T,%v,%p \n", d3, d3, &d3)
+	d3.name = "球球"
+	d3.color = "白色"
+	d3.kind = "萨摩耶"
+	fmt.Println("d3修改后:", d3)
+	fmt.Println("d1: ", d1)
+	fmt.Println("-------------")
+	//3.实现结构体浅拷贝：通过new()函数来实例化对象
+	d4 := new(Dog)
+	d4.name = "多多"
+	d4.color = "棕色"
+	d4.age = 1
+	d4.kind = "巴哥犬"
+	d5 := d4 // new() 返回的是 *Dog，因此这里复制的是指针（地址），d4 和 d5 指向同一个 Dog 对象
+	fmt.Printf("d4:%T,%v,%p \n", d4, d4, d4)
+	fmt.Printf("d5:%T,%v,%p \n", d5, d5, d5)
+	fmt.Println("--------------")
+	d5.color = "金色"
+	d5.kind = "金毛"
+	fmt.Println("d5修改后: ", d5)
+	fmt.Println("d4: ", d4)
+}
