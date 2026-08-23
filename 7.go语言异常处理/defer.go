2e435a7f8e5e4a25a2715b10d6dfe1ae59e3ccf2
patrick <patrick@qq.com>
2026-08-23 15:12:03 +0800

    defer 参数

--- 7.go语言异常处理/defer.go
+++ 7.go语言异常处理/defer.go
@@ -66,3 +66,23 @@ func deferInmethod() {
 	defer p.fullName()
 	fmt.Print("Wellcome ")
 }
+
+/*
+defer参数
+延迟函数的参数在执行延迟语句时被执行，而不是在执行实际的函数调用时执行
+*/
+func DeferParameter() {
+	a := 5
+	b := 6
+	defer printAdd(a, b, true)
+	a = 10
+	b = 7
+	printAdd(a, b, false)
+}
+func printAdd(a, b int, flag bool) {
+	if flag {
+		fmt.Printf("延迟执行函数printAdd(),参数a,b分别为%d,%d,两数之和为:%d\n", a, b, a+b)
+	} else {
+		fmt.Printf("未延迟执行函数printAdd(),参数a,b分别为%d,%d,两数之和为:%d\n", a, b, a+b)
+	}
+}
