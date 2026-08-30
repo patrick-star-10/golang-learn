76e2a60fc4c5f5449bf6cc4518e529d1b2524b96
patrick <2046390698@qq.com>
2026-08-30 15:34:38 +0800

    After函数

--- 8.go语言的并发编程/time包中与channel相关的函数.go
+++ 8.go语言的并发编程/time包中与channel相关的函数.go
@@ -22,4 +22,27 @@ func Timecount() {
 	fmt.Printf("%T\n", timer1.C)
 	fmt.Printf("%T\n", data)
 	fmt.Println(data)
+	/*
+	   Timer（计时器）本质上是基于 Channel 实现的。time.NewTimer(d) 创建一个计时器，
+	   并返回包含只读 C 通道的 Timer 对象。程序执行到 <-timer.C 时会阻塞等待，直到计时结束，
+	   Timer 自动向 C 通道发送一个 time.Time 类型的数据（触发时间），
+	   程序收到数据后继续执行。因此，Timer 可以看作是利用 Channel 实现的“时间通知器”
+	*/
 }
+
+/*
+	After()函数
+	After()函数相当于NewTimer(d).C
+*/
+//After()函数案例
+func after() {
+	//使用After(),返回值<-chan Time,同Timer.C
+	ch1 := time.After(5 * time.Second)
+	fmt.Println(time.Now())
+	data := <-ch1
+	fmt.Printf("%T\n", data)
+	fmt.Println(data)
+}
+
+//time.After() 可以理解为 time.NewTimer(d).C 的简化写法。
+// 只需要等待一次就用 After()；如果需要停止、重置或管理计时器，就使用 NewTimer()。
