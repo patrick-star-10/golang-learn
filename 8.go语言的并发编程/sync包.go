ed5856505212a006118991a559b458999a0ad54f
patrick <2046390698@qq.com>
2026-08-31 17:18:17 +0800

    mutex互斥锁

--- 8.go语言的并发编程/sync包.go
+++ 8.go语言的并发编程/sync包.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -38,4 +39,48 @@ func printnum(wg *sync.WaitGroup, num int) {
 	wg.Done() //计数器减1
 	// Add(3) 表示我要等 3 个 goroutine；每个 goroutine 完成后 Done() 一次；
 	// main 在 Wait() 阻塞，直到 3 次 Done() 把计数器减到 0
+}
+
+/*
+	互斥锁
+	Mutex是一个互斥锁，可以创建为其他结构体的字段；零值为解锁状态
+	Mutex类型的锁和goroutine无关，可以由不同的goroutine加锁和解锁
+	Mutex中的方法如下：
+	func(m *Mutex)Lock()
+	Lock()方法锁住m,如果m已经加锁，则阻塞直到m解锁：func(m *Mutex)Unlock()
+	Unlock()方法解锁m,如果m未加锁就会导致运行时错误
+*/
+//通过互斥锁实现售票
+var tickets int = 20
+var wg sync.WaitGroup
+var mutex sync.Mutex
+
+func MutexLock() {
+	wg.Add(4)
+	go saleTickets("1号窗口", &wg)
+	go saleTickets("2号窗口", &wg)
+	go saleTickets("3号窗口", &wg)
+	go saleTickets("4号窗口", &wg)
+	wg.Wait()
+	defer fmt.Println("所有车票售空")
+}
+func saleTickets(name string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		mutex.Lock()
+		if tickets > 0 {
+			time.Sleep(1 * time.Second)
+			//获取窗口的编号
+			num, _ := strconv.Atoi(name[:1])
+			pre := strings.Repeat("-------", num)
+			fmt.Println(pre, name, tickets)
+			tickets--
+		} else {
+			fmt.Printf("%s结束售票\n", name)
+			mutex.Unlock()
+			break
+		}
+		//解锁
+		mutex.Unlock()
+	}
 }
